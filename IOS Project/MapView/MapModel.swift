b2285160a7ae23aeaec7ec05fdefc90de9cc652b
srutherhub <samuelbrutherford@gmail.com>
2025-05-06 12:00:37 -0500

    Init project: Waldi

---
+++ IOS Project/MapView/MapModel.swift
@@ -0,0 +1,8 @@
+//
+//  MapModel.swift
+//  IOS Project
+//
+//  Created by Samuel Rutherford on 5/4/25.
+//
+
+import Foundation
---
+++ IOS Project/MenuView/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  IOS Project
+//
+//  Created by Samuel Rutherford on 5/4/25.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
